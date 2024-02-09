9ca58ec10d8f15d5158b2d2170c5e10ab5d1fc0c
Alexander Vinogradow <alukard981@gmail.com>
2024-02-09 17:30:44 +0300

    added an opportunity to choose greet or exit options in App class (1- Greet 0- Exit)

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,18 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.userGreet();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter the game number and press Enter. \n 1. Greet \n 0. Exit \n Your choice: ");
+        String gameNumber = scanner.next();
+
+        if (gameNumber.equals("1")) {
+            Cli.userGreet();
+        } else {
+            System.out.println("Exit was choosen");
+            scanner.close();
+        }
     }
 }
