e906b1433b78dea80a0c4ca6256fd9b9c5fee177
Alexander Vinogradow <alukard981@gmail.com>
2024-02-09 15:06:17 +0300

    created Cli class with imported Scanner package so user can interact with console

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void userGreet() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("May i have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+        scanner.close();
+    }
+}
