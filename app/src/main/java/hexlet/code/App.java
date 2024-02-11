6a0f2176ca388bafe19f2517e2ffcc91d144278a
Alexander Vinogradow <alukard981@gmail.com>
2024-02-11 18:52:33 +0300

    invitation message was shortened

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please enter the game number and press Enter. \n 1. Greet \n 2. Even \n 0. Exit \n Your choice: ");
+        System.out.print("Please enter the game number and press Enter. ");
+        System.out.print("\n 1. Greet \n 2. Even \n 0. Exit \n Your choice: ");
         int gameNumber = scanner.nextInt();
 
         switch (gameNumber) {
