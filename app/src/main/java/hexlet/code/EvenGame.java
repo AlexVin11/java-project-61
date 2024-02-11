d0f54cece9562eec2ced3d8c2723b1afefa79734
Alexander Vinogradow <alukard981@gmail.com>
2024-02-11 18:38:08 +0300

    edited EvenGame to use user name

---
+++ app/src/main/java/hexlet/code/EvenGame.java
@@ -0,0 +1,47 @@
+package hexlet.code;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class EvenGame {
+    public static void evenGame(String userName) {
+        int winningCount = 0;
+        Scanner scanner = new Scanner(System.in);
+        Random r = new Random();
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+        while (winningCount < 3) {
+            int randomNumber = r.nextInt(100) + 1;
+            boolean isEvenRandomNumber = (randomNumber % 2) == 0;
+            System.out.println("Question: " + randomNumber);
+            System.out.println("Your answer is: ");
+            String userAnswer = scanner.next();
+
+            if ((isEvenRandomNumber && userAnswer.equalsIgnoreCase("yes")) ||
+            ((!isEvenRandomNumber) && userAnswer.equalsIgnoreCase("no"))) {
+                winningCount += 1;
+                System.out.println("Correct!");
+            } else if (isEvenRandomNumber && userAnswer.equalsIgnoreCase("no")) {
+                System.out.println(userAnswer + " is wrong answer ;(. Correct answer was 'yes'");
+                break;
+            } else if ((!isEvenRandomNumber) && userAnswer.equalsIgnoreCase("yes")) {
+                System.out.println(userAnswer + " is wrong answer ;(. Correct answer was 'no'");
+                break;
+            } else {
+                if (isEvenRandomNumber) {
+                    System.out.println(userAnswer + " is wrong answer ;(. Correct answer was 'yes'");
+                    break;
+                } else {
+                    System.out.println(userAnswer + " is wrong answer ;(. Correct answer was 'no'");
+                    break;
+                }
+            }
+        }
+
+        if (winningCount == 3) {
+            System.out.println("Congratulation, " + userName + " !");
+        } else {
+            System.out.println("Let`s try again " + userName);
+        }
+    }
+}
