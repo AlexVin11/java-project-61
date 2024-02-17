aea08fdae5f83be32fadd92aa8c8efcbd9f00718
Alexander Vinogradow <alukard981@gmail.com>
2024-02-18 00:53:38 +0300

    static final int variable used

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -4,17 +4,17 @@ import java.util.Random;
 
 public class Even {
     public static String evenGameRules = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+    static final int stringsInArray = 3;
+    static final int columnsInArray = 2;
+    static final int maxRandomNumberBorder = 100;
     public static String[][] even() {
         Random r = new Random();
         //2d massive 3 lines 2 columns
         //first column contains game expression that will be shown to user
         //second column contains correct answer
-        int stringsInArray = 3;
-        int columnsInArray = 2;
         String[][] questionAndCorrectAnswer = new String[stringsInArray][columnsInArray];
 
         for (int i = 0; i < stringsInArray; i++) {
-            int maxRandomNumberBorder = 100;
             //random number from 1 to 100 generating
             int randomNumber = r.nextInt(maxRandomNumberBorder) + 1;
             //number to string convertation
