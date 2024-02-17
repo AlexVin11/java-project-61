e77f1b8bef6c80358c68b4242c989b340990ef45
Alexander Vinogradow <alukard981@gmail.com>
2024-02-17 19:19:29 +0300

    cheerup message changed( case: user gave a wrong answer)

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -41,7 +41,7 @@ public class Engine {
         } else {
             System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was "
                     + "'" + arguments[i][1] + "'");
-            System.out.println("Lets try again, " + userName + "!");
+            System.out.println("Let's try again, " + userName + "!");
         }
     }
 }
