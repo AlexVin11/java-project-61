243cc713392d8799732ea0e8632a2b922852cce2
Alexander Vinogradow <alukard981@gmail.com>
2024-02-21 18:21:16 +0300

    method name now verb (was noun)

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static String userGreet() {
+    public static String userGreeting() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
