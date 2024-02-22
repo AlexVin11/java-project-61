6e4520595b88fa28d7b666cb1a648d52eef484d2
Alexander Vinogradow <alukard981@gmail.com>
2024-02-22 20:38:07 +0300

    game class now has 3 methods (1 for generating expression and correct answer, 2 for collecting it in array for 3 games, 3 for call engine with arguments needed)

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -7,57 +7,76 @@ import java.util.Random;
 
 public class Progression {
     private static String progressionGameRules = "What number is missing in the progression?";
-    static final int STRINGSINARRAY = 3;
-    static final int COLUMNSINARRAY = 2;
+    static final int STRINGSINRESULTARRAY = 3;
+    static final int COLUMNSINRESULTARRAY = 2;
     static final int MAXNUMBEROFITEMS = 10;
     static final int MINNUMBEROFITEMS = 5;
     static final int MAXPROGRESSIONDEPENDENCYBORDER = 10;
     static final int MAXFIRSTNUMBERBORDER = 20;
+    static final int STRINGSINGENERATEDARRAY = 1;
+    static final int COLUMNSINGENERATEDARRAY = 2;
 
-    public static void progression() {
+    public static String[][] generatingProgressionExpression() {
         Random r = new Random();
+        String[][] expressionAndCorrectAnswer = new String[STRINGSINGENERATEDARRAY][COLUMNSINGENERATEDARRAY];
+        //random number from 5 to 9 (inclusive)
+        //length of resultProgression massive
+        int lengthOfProgression = r.nextInt(MAXNUMBEROFITEMS - MINNUMBEROFITEMS) + MINNUMBEROFITEMS;
+        int[] resultProgression = new int[lengthOfProgression];
+        int progressionDependency = r.nextInt(MAXPROGRESSIONDEPENDENCYBORDER) + 1;
+        //first element is inclusive, last element is exclusive
+        int indexOfHidedElement = r.nextInt(lengthOfProgression);
+        //first number in resultProgression (number generated from 1 to 20)
+        int firstNumberOfMassive = r.nextInt(MAXFIRSTNUMBERBORDER) + 1;
+        resultProgression[0] = firstNumberOfMassive;
+        String[] processedProgressionMassive = new String[lengthOfProgression];
+
+        for (int j = 1; j < lengthOfProgression; j++) {
+            resultProgression[j] = resultProgression[j - 1] + progressionDependency;
+        }
+
+        for (int k = 0; k < lengthOfProgression; k++) {
+            int progressionElement = resultProgression[k];
+            String processedElement = Integer.toString(progressionElement);
+
+            if (k == indexOfHidedElement) {
+                processedProgressionMassive[k] = "..";
+            } else {
+                processedProgressionMassive[k] = processedElement;
+            }
+        }
+
+        String progressionMassive = Arrays.toString(processedProgressionMassive);
+        String processedMassive = progressionMassive.replace("[", "");
+        String progression = processedMassive.replace("]", "");
+        String resultExpression = progression.replace(",", "");
+        String correctAnswer = Integer.toString(resultProgression[indexOfHidedElement]);
+        expressionAndCorrectAnswer[0][0] = resultExpression;
+        expressionAndCorrectAnswer[0][1] = correctAnswer;
+
+        return expressionAndCorrectAnswer;
+    }
+
+    public static String[][] progressionResultArrayCreating() {
         //2d massive 3 lines 2 columns
         //first column contains game expression that will be shown to user
         //second column contains correct answer
-        String[][] questionAndCorrectAnswer = new String[STRINGSINARRAY][COLUMNSINARRAY];
+        String[][] questionAndCorrectAnswer = new String[STRINGSINRESULTARRAY][COLUMNSINRESULTARRAY];
+        int i = 0;
 
-        for (int i = 0; i < STRINGSINARRAY; i++) {
-            //random number from 5 to 9 (inclusive)
-            //length of resultProgression massive
-            int lengthOfProgression = r.nextInt(MAXNUMBEROFITEMS - MINNUMBEROFITEMS) + MINNUMBEROFITEMS;
-            int[] resultProgression = new int[lengthOfProgression];
-            int progressionDependency = r.nextInt(MAXPROGRESSIONDEPENDENCYBORDER) + 1;
-            //first element is inclusive, last element is exclusive
-            int indexOfHidedElement = r.nextInt(lengthOfProgression);
-            //first number in resultProgression (number generated from 1 to 20)
-            int firstNumberOfMassive = r.nextInt(MAXFIRSTNUMBERBORDER) + 1;
-            resultProgression[0] = firstNumberOfMassive;
-            String[] processedProgressionMassive = new String[lengthOfProgression];
-
-            for (int j = 1; j < lengthOfProgression; j++) {
-                resultProgression[j] = resultProgression[j - 1] + progressionDependency;
-            }
-
-            for (int k = 0; k < lengthOfProgression; k++) {
-                int progressionElement = resultProgression[k];
-                String processedElement = Integer.toString(progressionElement);
-
-                if (k == indexOfHidedElement) {
-                    processedProgressionMassive[k] = "..";
-                } else {
-                    processedProgressionMassive[k] = processedElement;
-                }
-            }
-
-            String progressionMassive = Arrays.toString(processedProgressionMassive);
-            String processedMassive = progressionMassive.replace("[", "");
-            String progression = processedMassive.replace("]", "");
-            String resultExpression = progression.replace(",", "");
-            String correctAnswer = Integer.toString(resultProgression[indexOfHidedElement]);
-            questionAndCorrectAnswer[i][0] = resultExpression;
-            questionAndCorrectAnswer[i][1] = correctAnswer;
+        while (i < STRINGSINRESULTARRAY) {
+            String[][] processedArray = new String[1][2];
+            processedArray = generatingProgressionExpression();
+            questionAndCorrectAnswer[i][0] = processedArray[0][0];
+            questionAndCorrectAnswer[i][1] = processedArray[0][1];
+            i++;
         }
 
+        return questionAndCorrectAnswer;
+    }
+
+    public static void progression1GameRules() {
+        String[][] questionAndCorrectAnswer = progressionResultArrayCreating();
         Engine.engine(questionAndCorrectAnswer, progressionGameRules);
     }
 }
