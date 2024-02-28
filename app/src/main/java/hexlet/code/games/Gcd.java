83f1b5853a1693e9751886559d96a6f0272d3a6e
Alexander Vinogradow <alukard981@gmail.com>
2024-02-28 23:38:34 +0300

    improvements after 2 review made

--- app/src/main/java/hexlet/code/games/Gcd.java
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -1,60 +1,61 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-
-import java.util.Random;
+import hexlet.code.Utils;
 
 public class Gcd {
     private static final String GCDGAMERULES = "Find the greatest common divisor of given numbers.";
-    static final int STRINGSINRESULTARRAY = 3;
-    static final int COLUMNSINRESULTARRAY = 2;
-    static final int BORDEROFRANDOMNUMBER = 20;
+    static final int MAXRANDOMNUMBER = 20;
+    static final int MINRANDOMNUMBER = 1;
     static final int STRINGSINGENERATEDARRAY = 1;
     static final int COLUMNSINGENERATEDARRAY = 2;
-    public static String[][] generatingGcdExpression() {
-        Random r = new Random();
+    public static int generatingCorrectGcdAnswer(int firstNumber, int secondNumber) {
+        int gcdOfNumbers = 1;
+
+        for (int j = 1; j <= firstNumber && j <= secondNumber; j++) {
+            if (firstNumber % j == 0 && secondNumber % j == 0) {
+                gcdOfNumbers = j;
+            }
+        }
+
+        return gcdOfNumbers;
+    }
+
+    public static String[][] gcdArrayCreating() {
+        //2d massive 1 lines 2 columns
+        //first column contains game expression that will be shown to user
+        //second column contains correct answer
         String[][] expressionAndCorrectAnswer = new String[STRINGSINGENERATEDARRAY][COLUMNSINGENERATEDARRAY];
-        //random numbers from 1 to 50 generated and converted to String
-        int firstNumber = r.nextInt(BORDEROFRANDOMNUMBER) + 1;
-        int secondNumber = r.nextInt(BORDEROFRANDOMNUMBER) + 1;
+        //random numbers from 1 to 20 generated and converted to String
+        int firstNumber = Utils.generateRandomInt(MINRANDOMNUMBER, MAXRANDOMNUMBER);
+        int secondNumber = Utils.generateRandomInt(MINRANDOMNUMBER, MAXRANDOMNUMBER);
         String processedFirstNumber = Integer.toString(firstNumber);
         String processedSecondNumber = Integer.toString(secondNumber);
+        int correctAnswer = generatingCorrectGcdAnswer(firstNumber, secondNumber);
+        String processedCorrectAnswer = Integer.toString(correctAnswer);
         var expressionToCalculate = new StringBuilder();
+
         expressionToCalculate.append(processedFirstNumber);
         expressionToCalculate.append(" ");
         expressionToCalculate.append(processedSecondNumber);
         String expression = expressionToCalculate.toString();
         expressionAndCorrectAnswer[0][0] = expression;
-
-        for (int j = 1; j <= firstNumber && j <= secondNumber; j++) {
-            if (firstNumber % j == 0 && secondNumber % j == 0) {
-                expressionAndCorrectAnswer[0][1] = Integer.toString(j);
-            }
-        }
+        expressionAndCorrectAnswer[0][1] = processedCorrectAnswer;
 
         return expressionAndCorrectAnswer;
     }
 
-    public static String[][] gcdResultArrayCreating() {
-        //2d massive 3 lines 2 columns
-        //first column contains game expression that will be shown to user
-        //second column contains correct answer
-        String[][] questionAndCorrectAnswer = new String[STRINGSINRESULTARRAY][COLUMNSINRESULTARRAY];
+    public static void gcdGameExecuting() {
+        String[][] questionAndCorrectAnswer = new String[Engine.GAMESTOWIN][COLUMNSINGENERATEDARRAY];
         int i = 0;
 
-        while (i < STRINGSINRESULTARRAY) {
-            String[][] processedArray = new String[1][2];
-            processedArray = generatingGcdExpression();
+        while (i < Engine.GAMESTOWIN) {
+            String[][] processedArray = gcdArrayCreating();
             questionAndCorrectAnswer[i][0] = processedArray[0][0];
             questionAndCorrectAnswer[i][1] = processedArray[0][1];
             i++;
         }
 
-        return questionAndCorrectAnswer;
-    }
-
-    public static void gcdGameExecuting() {
-        String[][] questionAndCorrectAnswer = gcdResultArrayCreating();
         Engine.engine(questionAndCorrectAnswer, GCDGAMERULES);
     }
 }
