0513e77b225111ecf0ba376c8ec6e1dc3788e42a
Alexander Vinogradow <alukard981@gmail.com>
2024-03-05 13:49:05 +0300

    gameExecuting part refactored to for from while

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -62,13 +62,11 @@ public class Calc {
 
     public static void calcGameExecuting() {
         String[][] questionAndCorrectAnswer = new String[Engine.GAMESTOWIN][COLUMNSINGENERATEDARRAY];
-        int i = 0;
 
-        while (i < Engine.GAMESTOWIN) {
+        for (int i = 0; i < Engine.GAMESTOWIN; i++) {
             String[][] processedArray = calcArrayCreating();
             questionAndCorrectAnswer[i][0] = processedArray[0][0];
             questionAndCorrectAnswer[i][1] = processedArray[0][1];
-            i++;
         }
 
         Engine.engine(questionAndCorrectAnswer, CALCGAMERULES);
