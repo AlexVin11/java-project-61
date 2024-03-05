907ecd5fddd7ae554b8557eb0df1fdd3bec2d0f3
Alexander Vinogradow <alukard981@gmail.com>
2024-03-05 13:51:53 +0300

    gameExecuting part refactored to for from while

--- app/src/main/java/hexlet/code/games/Gcd.java
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -47,13 +47,11 @@ public class Gcd {
 
     public static void gcdGameExecuting() {
         String[][] questionAndCorrectAnswer = new String[Engine.GAMESTOWIN][COLUMNSINGENERATEDARRAY];
-        int i = 0;
 
-        while (i < Engine.GAMESTOWIN) {
+        for (int i = 0; i < Engine.GAMESTOWIN; i++) {
             String[][] processedArray = gcdArrayCreating();
             questionAndCorrectAnswer[i][0] = processedArray[0][0];
             questionAndCorrectAnswer[i][1] = processedArray[0][1];
-            i++;
         }
 
         Engine.engine(questionAndCorrectAnswer, GCDGAMERULES);
