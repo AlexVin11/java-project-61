78ff0293050cfee85178946c5e8b6154feb0fcaf
Alexander Vinogradow <alukard981@gmail.com>
2024-03-12 16:25:02 +0300

    empty lines removed, winning count magic number variable removed (no i is comparing to GAMESTOWIN)

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -7,36 +7,29 @@ public class Engine {
 
     public static void engine(String[][] arguments, String gameRules) {
         Scanner scanner = new Scanner(System.in);
-        int winningCount = 0;
         String brainGamesGreetingMessage = "Welcome to the Brain Games!\nMay i have your name? ";
         String userName = "";
         String userAnswer = "";
         String answerRequest = "Your answer: ";
         String correctAnswerNotification = "Correct!";
-
         System.out.print(brainGamesGreetingMessage);
         userName += scanner.next();
         System.out.println("Hello, " + userName + "!");
         System.out.println(gameRules);
-
         for (int i = 0; i < GAMESTOWIN; i++) {
             System.out.println("Question: " + arguments[i][0]);
             System.out.print(answerRequest);
             userAnswer = scanner.next();
-
             if (userAnswer.equalsIgnoreCase(arguments[i][1])) {
-                winningCount += 1;
                 System.out.println(correctAnswerNotification);
             }
-
             if (!userAnswer.equalsIgnoreCase(arguments[i][1])) {
                 System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was "
                         + "'" + arguments[i][1] + "'.");
                 System.out.println("Let's try again, " + userName + "!");
                 break;
             }
-
-            if (winningCount == GAMESTOWIN) {
+            if (i == GAMESTOWIN) {
                 System.out.println("Congratulations, " + userName + "!");
                 break;
             }
